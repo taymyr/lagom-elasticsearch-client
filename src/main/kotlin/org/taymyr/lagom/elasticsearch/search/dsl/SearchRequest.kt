d401030742fae75777f09f55882c1e7fd9588cc2
Ilya Korshunov <admin@pjandro.ru>
2018-10-19 17:23:56 +0300

    Add post_filter for SearchRequest Use JvmField instead JvmStatic for static fields in MappingProperty (now from java we can use MappingProperty.LONG instead MappingProperty.getLONG())

--- src/main/kotlin/org/taymyr/lagom/elasticsearch/search/dsl/SearchRequest.kt
+++ src/main/kotlin/org/taymyr/lagom/elasticsearch/search/dsl/SearchRequest.kt
@@ -1,5 +1,6 @@
 package org.taymyr.lagom.elasticsearch.search.dsl
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.taymyr.lagom.elasticsearch.search.dsl.query.Query
 import org.taymyr.lagom.elasticsearch.search.dsl.query.aggregation.Aggregation
 
@@ -12,5 +13,7 @@ data class SearchRequest @JvmOverloads constructor(
     val query: Query,
     val from: Int? = null,
     val size: Int? = null,
-    val aggs: Map<String, Aggregation>? = null
+    val aggs: Map<String, Aggregation>? = null,
+    @JsonProperty("post_filter")
+    val postFilter: Query? = null
 )
