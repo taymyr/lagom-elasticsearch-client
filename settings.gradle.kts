87f9726cc14fd070d1156c71c297cb580f7f5658
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-19 15:20:53 +0800

    Delete settings.gradle.kts

--- settings.gradle.kts
+++
@@ -1,5 +0,0 @@
-include("java", "scala")
-
-rootProject.name = "lagom-elasticsearch-client"
-project(":java").name = "lagom-elasticsearch-client-java"
-project(":scala").name = "lagom-elasticsearch-client-scala"
